ec7e589b7adb2b923eb6474334e01f61a22d482a
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-05-09 09:56:46 +0200

    SQLite creacion de tabla

--- sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
+++ sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
@@ -3,6 +3,7 @@ package sqlitejdbcdriverconnection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SQLiteJDBCDriverConnection {
 
@@ -29,10 +30,30 @@ public class SQLiteJDBCDriverConnection {
         }
     }
 
+        public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:tests.db";
+        
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS clase ("
+                + "	id integer PRIMARY KEY,"
+                + "	name text NOT NULL,"
+                + "	secondname text NOT NULL"
+                + ");";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         connect();
+        createNewTable();
     }
 }
